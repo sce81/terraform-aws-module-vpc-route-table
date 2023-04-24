9f0fe12aaa66c3aa21d8d9b7e19e424c61f7e5ce
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-24 15:29:00 +0100

    develop - changed to origin style

--- data.tf
+++ data.tf
@@ -5,26 +5,8 @@ data "aws_vpc" "main" {
   }
 }
 
-data "aws_subnets" "main" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["*-${var.subnet_name}-*"]
-  }
-}
-
-data "aws_subnet" "main" {
-  for_each = data.aws_subnets.main.ids
-  id       = each.value
-}
-
 locals {
 
-  subnet_ids = tolist(data.aws_subnet.main.ids)
 
   common_tags = tomap({
     Terraform = "true"
