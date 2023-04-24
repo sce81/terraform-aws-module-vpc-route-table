ef4a457031f232c4b6965975b4cabbf9feec4f96
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-24 14:54:45 +0100

    develop - naming refinements

--- data.tf
+++ data.tf
@@ -1,11 +1,29 @@
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.name}-*"]
+  }
+}
+
+//data "aws_subnets" "main" {
+//  filter {
+//    name   = "vpc-id"
+//    values = [data.aws_vpc.main.id]
+//  }
+//
+//  filter {
+//    name   = "tag:Name"
+//    values = ["*-${var.subnet_name}-*"]
+//  }
+//}
+
 locals {
+
+  subnet_ids = tolist(var.subnet_ids)
+
   common_tags = tomap({
-    Terraform   = "true"
+    Terraform = "true"
   })
 
-}
 
-data "aws_vpc" "main" {
-  id = var.vpc_id
 }
-
--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 resource "aws_route_table" "main" {
-    count                               = var.routes_count
-    vpc_id                              = var.vpc_id
+  count  = var.routes_count
+  vpc_id = var.vpc_id
 
-    tags = merge(
-        local.common_tags, var.extra_tags,
-        tomap({
-            Name = "${element(var.route_name, count.index)}"
-        })
-    )
+  tags = merge(
+    local.common_tags, var.extra_tags,
+    tomap({
+      Name = "${element(var.route_name, count.index)}"
+    })
+  )
 }
 
 resource "aws_route" "main" {
- count                                  = length(var.route_info) 
+  count = length(var.route_info)
 
- route_table_id                         = element(aws_route_table.main[*].id, count.index)
- destination_cidr_block                 = var.route_info[count.index].route_cidr
- egress_only_gateway_id                 = var.route_info[count.index].egress_only_gateway_id
- gateway_id                             = var.route_info[count.index].gateway_id
- instance_id                            = var.route_info[count.index].instance_id
- nat_gateway_id                         = var.route_info[count.index].nat_gateway_id
- network_interface_id                   = var.route_info[count.index].network_interface_id
- transit_gateway_id                     = var.route_info[count.index].transit_gateway_id
- vpc_peering_connection_id              = var.route_info[count.index].vpc_peering_connection_id
- 
+  route_table_id            = element(aws_route_table.main[*].id, count.index)
+  destination_cidr_block    = var.route_info[count.index].route_cidr
+  egress_only_gateway_id    = var.route_info[count.index].egress_only_gateway_id
+  gateway_id                = var.route_info[count.index].gateway_id
+  instance_id               = var.route_info[count.index].instance_id
+  nat_gateway_id            = var.route_info[count.index].nat_gateway_id
+  network_interface_id      = var.route_info[count.index].network_interface_id
+  transit_gateway_id        = var.route_info[count.index].transit_gateway_id
+  vpc_peering_connection_id = var.route_info[count.index].vpc_peering_connection_id
+
 }
- 
+
 
 resource "aws_route_table_association" "main" {
-    count                               = length(var.subnet_ids)
+  count = length(local.subnet_ids)
 
-    subnet_id                           = element(var.subnet_ids, count.index)
-    route_table_id                      = element(aws_route_table.main[*].id, count.index)
+  subnet_id      = element(local.subnet_ids, count.index)
+  route_table_id = element(aws_route_table.main[*].id, count.index)
 }
